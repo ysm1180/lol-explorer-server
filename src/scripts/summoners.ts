0d0b74699024f3ca261e1cbebbca8d8777c58cb8
Seongmin Yeon <ysm1180@gmail.com>
2019-07-15 11:24:42 +0900

    Modify getting summoner scripts structure

--- src/scripts/summoners.ts
+++ src/scripts/summoners.ts
@@ -2,33 +2,21 @@ import axios from 'axios';
 import * as express from 'express';
 import mongo from '../db/mongo';
 import { Demacia } from '../lib/demacia/demacia';
-import { STRATEGY } from '../lib/demacia/ratelimiter/ratelimiter';
+import DevApi from '../models/statistics/api';
 import StatisticsSummoner from '../models/statistics/summoner';
-import { apiKey } from './api';
+import devApi, { IDevApiClassData } from './api';
 
 const app = express();
 mongo.connect();
 
-const demacia = [
-  new Demacia(apiKey[0], STRATEGY.SPREAD),
-  new Demacia(apiKey[1], STRATEGY.SPREAD),
-  new Demacia(apiKey[2], STRATEGY.SPREAD),
-  new Demacia(apiKey[3], STRATEGY.SPREAD),
-  new Demacia(apiKey[4], STRATEGY.SPREAD),
-];
-
-const initSummonerList = async () => {
+const insertHighRankSummonerList = async (demacia: Demacia) => {
   try {
-    console.log('START');
-
-    await StatisticsSummoner.remove({});
-
     const result = [];
     const queues: ('RANKED_SOLO_5x5' | 'RANKED_FLEX_SR')[] = ['RANKED_SOLO_5x5', 'RANKED_FLEX_SR'];
     for (let i = 0; i < queues.length; i++) {
       const queue = queues[i];
 
-      let challengers = await demacia[0].getChallengerSummonerList(queue);
+      let challengers = await demacia.getChallengerSummonerList(queue);
       for (let i = 0; i < challengers.entries.length; i++) {
         result.push({
           name: challengers.entries[i].summonerName,
@@ -37,9 +25,9 @@ const initSummonerList = async () => {
           rank: '',
         });
       }
-      console.log(`${queue} challengers ${challengers.entries.length}`);
+      console.log(`${queue} Challengers ${challengers.entries.length}`);
 
-      let grandmasters = await demacia[0].getGrandMasterSummonerList(queue);
+      let grandmasters = await demacia.getGrandMasterSummonerList(queue);
       for (let i = 0; i < grandmasters.entries.length; i++) {
         result.push({
           name: grandmasters.entries[i].summonerName,
@@ -48,9 +36,9 @@ const initSummonerList = async () => {
           rank: '',
         });
       }
-      console.log(`${queue} grandmasters ${grandmasters.entries.length}`);
+      console.log(`${queue} Grandmasters ${grandmasters.entries.length}`);
 
-      let masters = await demacia[0].getMasterSummonerList(queue);
+      let masters = await demacia.getMasterSummonerList(queue);
       for (let i = 0; i < masters.entries.length; i++) {
         result.push({
           name: masters.entries[i].summonerName,
@@ -59,94 +47,135 @@ const initSummonerList = async () => {
           rank: '',
         });
       }
-      console.log(`${queue} masters ${masters.entries.length}`);
+      console.log(`${queue} Masters ${masters.entries.length}`);
     }
 
     await StatisticsSummoner.insertMany(result);
 
-    const ranks = ['I', 'II', 'III', 'IV'];
+    return;
+  } catch (err) {
+    return Promise.reject(err);
+  }
+};
+
+const getSummonerList = async (
+  queue: 'RANKED_SOLO_5x5' | 'RANKED_FLEX_SR',
+  tier: string,
+  rank: string,
+  page: number,
+  demacia: Demacia
+) => {
+  try {
+    let summoners = await demacia.getSummonerListByLeague(queue, tier, rank, page);
+
+    const result = [];
+    for (let i = 0; i < summoners.length; i++) {
+      result.push({
+        name: summoners[i].summonerName,
+        queue,
+        tier,
+        rank,
+      });
+    }
+
+    return result;
+  } catch (err) {
+    return Promise.reject(err);
+  }
+};
+
+DevApi.find().then(async (data) => {
+  try {
+    const queues: ('RANKED_SOLO_5x5' | 'RANKED_FLEX_SR')[] = ['RANKED_SOLO_5x5', 'RANKED_FLEX_SR'];
     const tiers = ['DIAMOND', 'PLATINUM'];
+    const ranks = ['I', 'II', 'III', 'IV'];
 
-    let page = 1;
-    const getSummonerList = async (
-      queue: 'RANKED_SOLO_5x5' | 'RANKED_FLEX_SR',
-      tier: string,
-      rank: string,
-      index: number
-    ) => {
-      try {
-        let summoners = await demacia[index].getSummonerListByLeague(
-          queue,
-          tier,
-          rank,
-          page + index
-        );
+    const progressData: any[] = [];
+    for (let i = 0; i < queues.length; i++) {
+      const queue = queues[i];
 
-        const result = [];
-        for (let i = 0; i < summoners.length; i++) {
-          result.push({
-            name: summoners[i].summonerName,
+      for (let j = 0; j < tiers.length; j++) {
+        const tier = tiers[j];
+
+        for (let k = 0; k < ranks.length; k++) {
+          const rank = ranks[k];
+          progressData.push({
             queue,
             tier,
             rank,
+            page: 0,
           });
-        }
-
-        return result;
-      } catch (err) {
-        if (err.response) {
-          if (err.response.status === 403) {
-            axios.post('http://gasi.asuscomm.com:5001/expired', { api_key: apiKey[index] });
-            console.log(`EXPIRED ${index}: ${apiKey[index]}`);
-          } else {
-            console.log(err.response.data);
-          }
-        } else {
-          console.log(err);
-        }
-        return [];
-      }
-    };
-
-    for (let i = 0; i < queues.length; i++) {
-      const queue = queues[i];
-      for (let j = 0; j < tiers.length; j++) {
-        const tier = tiers[j];
-        for (let k = 0; k < ranks.length; k++) {
-          const rank = ranks[k];
-
-          page = 1;
-          while (1) {
-            const names = await Promise.all([
-              getSummonerList(queue, tier, rank, 0),
-              getSummonerList(queue, tier, rank, 1),
-              getSummonerList(queue, tier, rank, 2),
-              getSummonerList(queue, tier, rank, 3),
-              getSummonerList(queue, tier, rank, 4),
-            ]).then((names) => {
-              return [...names[0], ...names[1], ...names[2], ...names[3], ...names[4]];
-            });
-
-            if (names.length === 0) {
-              break;
-            }
-
-            await StatisticsSummoner.insertMany(names);
-            console.log(`${queue} ${tier} ${rank} ${names.length}`);
-
-            page += 5;
-          }
         }
       }
     }
 
-    console.log('End Inserting Summoners');
+    const keys = data.map((k) => k.key);
+    devApi.setExpiredFn(async (key: string) => {
+      try {
+        await axios.post('http://localhost:5555/expired', { api_key: key });
+        console.log(`EXPIRED ${key}`);
+        devApi.removeKey(key);
+      } catch (err) {
+        console.log(`Expired function error ${err}`);
+      }
+    });
+    devApi.setSharedData({
+      insertedHighRanks: false,
+      pageData: progressData,
+    });
+    devApi.setProcessFunction(async (sharedData: any, apiClassData: IDevApiClassData) => {
+      try {
+        if (!sharedData.insertedHighRanks) {
+          sharedData.insertedHighRanks = true;
+          await insertHighRankSummonerList(apiClassData.demacia);
+        }
+
+        let pageData = sharedData.pageData.filter((data: { page: number }) => data.page !== -1);
+        while (pageData.length > 0) {
+          const idx = Math.floor(Math.random() * pageData.length);
+          const randomData = pageData[idx];
+          if (randomData.page !== -1) {
+            const result = await getSummonerList(
+              randomData.queue,
+              randomData.tier,
+              randomData.rank,
+              randomData.page++,
+              apiClassData.demacia
+            );
+            if (result.length === 0) {
+              randomData.page = -1;
+            } else {
+              console.log(
+                `${randomData.queue} ${randomData.tier} ${randomData.rank} ${randomData.page}`
+              );
+              await StatisticsSummoner.insertMany(result);
+            }
+          }
+
+          pageData = sharedData.pageData.filter((data: { page: number }) => data.page !== -1);
+        }
+
+        console.log('END');
+      } catch (err) {
+        if (err.response && err.response.status === 403) {
+          return Promise.reject(err);
+        }
+
+        if (err.response) {
+          console.error(err.response.data);
+        } else {
+          console.error(err);
+        }
+      }
+    });
+
+    await StatisticsSummoner.remove({});
+    await devApi.runAll(keys);
+    return;
   } catch (err) {
-    console.log(err);
+    return Promise.reject(err);
   }
-};
+});
 
-initSummonerList();
-
-var port = process.env.PORT || 4001;
+var port = process.env.PORT || 6667;
 app.listen(port);
